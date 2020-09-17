a08cf70c39dfbdfcd934466b10007a99f707b881
sharul hafiz <sharulhafiz@gmail.com>
2020-09-17 15:59:13 +0800

    update publication

--- json_publication.php
+++ json_publication.php
@@ -7,7 +7,7 @@
 	}
 	
 	$nama = $q;
-	$query = "select DISTINCT SUB_JUDUL from VIEWTABLE.VW_SKUNK_RESEARCH_PUBLICATION where SUB_JUDUL LIKE '%".$q_caps."%' AND rownum <= 10";
+	$query = "select * from VIEWTABLE.VW_SKUNK_RESEARCH_PUBLICATION where NAMA LIKE '%".$q_caps."%' OR SUB_JUDUL LIKE '%".$q_caps."%' AND rownum <= 10";
 	
 	$stid = oci_parse($conn, $query);
 	oci_execute($stid);
